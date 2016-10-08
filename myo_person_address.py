77ce0b715ad3f251bd8c0883e9e3b9d32d41ac56
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-10-07 21:43:45 -0300

    [IMP] myo_person_address.py

--- myo_person_address.py
+++ myo_person_address.py
@@ -22,6 +22,7 @@
 from __future__ import print_function
 
 import sqlite3
+import re
 
 
 def person_address_export_sqlite(client, args, db_path, table_name):
@@ -109,3 +110,161 @@ def person_address_export_sqlite(client, args, db_path, table_name):
 
     print()
     print('--> person_address_count: ', person_address_count)
+
+
+def person_address_import_sqlite(client, args, db_path, table_name, tag_table_name, role_table_name, person_table_name, address_table_name):
+
+    person_model = client.model('myo.person.address')
+
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+    cursor2 = conn.cursor()
+
+    person_count = 0
+
+    data = cursor.execute(
+        '''
+        SELECT
+            id,
+            tag_ids,
+            person_id,
+            address_id,
+            role_id,
+            sign_in_date,
+            sign_out_date,
+            notes,
+            active,
+            active_log,
+            new_id
+        FROM ''' + table_name + ''';
+        '''
+    )
+
+    print(data)
+    print([field[0] for field in cursor.description])
+    for row in cursor:
+        person_count += 1
+
+        print(
+            person_count, row['id'], row['tag_ids'], row['person_id'], row['address_id'], row['role_id']
+        )
+
+        values = {
+            # 'tag_ids': row['tag_ids'],
+            # 'person_id': row['person_id'],
+            # 'address_id': row['address_id'],
+            # 'role_id': row['role_id'],
+            'sign_in_date': row['sign_in_date'],
+            'sign_out_date': row['sign_out_date'],
+            'notes': row['notes'],
+            'active': row['active'],
+            'active_log': row['active_log'],
+        }
+        person_id = person_model.create(values).id
+
+        cursor2.execute(
+            '''
+           UPDATE ''' + table_name + '''
+           SET new_id = ?
+           WHERE id = ?;''',
+            (person_id,
+             row['id']
+             )
+        )
+
+        if row['tag_ids'] != '[]':
+
+            tag_ids = row['tag_ids'].split(',')
+            new_tag_ids = []
+            for x in range(0, len(tag_ids)):
+                tag_id = int(re.sub('[^0-9]', '', tag_ids[x]))
+                cursor2.execute(
+                    '''
+                    SELECT new_id
+                    FROM ''' + tag_table_name + '''
+                    WHERE id = ?;''',
+                    (tag_id,
+                     )
+                )
+                new_tag_id = cursor2.fetchone()[0]
+
+                values = {
+                    'tag_ids': [(4, new_tag_id)],
+                }
+                person_model.write(person_id, values)
+
+                new_tag_ids.append(new_tag_id)
+
+            print('>>>>>', row[4], new_tag_ids)
+
+        if row['person_id']:
+
+            person_id = row['person_id']
+
+            cursor2.execute(
+                '''
+                SELECT new_id
+                FROM ''' + person_table_name + '''
+                WHERE id = ?;''',
+                (person_id,
+                 )
+            )
+            person_id = cursor2.fetchone()[0]
+
+            values = {
+                'person_id': person_id,
+            }
+            person_model.write(person_id, values)
+
+            print('>>>>>', row['person_id'], person_id)
+
+        if row['address_id']:
+
+            address_id = row['address_id']
+
+            cursor2.execute(
+                '''
+                SELECT new_id
+                FROM ''' + address_table_name + '''
+                WHERE id = ?;''',
+                (address_id,
+                 )
+            )
+            address_id = cursor2.fetchone()[0]
+
+            values = {
+                'address_id': address_id,
+            }
+            person_model.write(person_id, values)
+
+            print('>>>>>', row['address_id'], address_id)
+
+        if row['role_id']:
+
+            role_id = row['role_id']
+
+            cursor2.execute(
+                '''
+                SELECT new_id
+                FROM ''' + role_table_name + '''
+                WHERE id = ?;''',
+                (role_id,
+                 )
+            )
+            role_id = cursor2.fetchone()[0]
+
+            values = {
+                'role_id': role_id,
+            }
+            person_model.write(person_id, values)
+
+            print('>>>>>', row['role_id'], role_id)
+
+    conn.commit()
+    conn.close()
+
+    print()
+    print('--> person_count: ', person_count)
