e14c4c4dd826541d1e4d5e0fb791fb4477483bb9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-11-01 10:26:52 -0200

    [IMP] myo_community.py

--- myo_community.py
+++ myo_community.py
@@ -21,6 +21,8 @@
 
 from __future__ import print_function
 
+import sqlite3
+
 
 def community_create_community(client, community_name, responsible_name, department_name):
 
@@ -58,3 +60,103 @@ def community_create_community(client, community_name, responsible_name, departm
     print()
     print('--> Done')
     print()
+
+
+def community_export_sqlite(client, args, db_path, table_name):
+
+    conn = sqlite3.connect(db_path)
+    conn.text_factory = str
+
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''DROP TABLE ''' + table_name + ''';''')
+    except Exception as e:
+        print('------->', e)
+    cursor.execute(
+        '''
+        CREATE TABLE ''' + table_name + ''' (
+            id INTEGER NOT NULL PRIMARY KEY,
+            tag_ids,
+            category_ids,
+            parent_id,
+            name,
+            alias,
+            code,
+            comm_location,
+            date_inclusion,
+            user_id,
+            notes,
+            active,
+            active_log,
+            new_id INTEGER
+            );
+        '''
+    )
+
+    community_model = client.model('myo.community')
+    community_browse = community_model.browse(args)
+
+    community_count = 0
+    for community_reg in community_browse:
+        community_count += 1
+
+        print(community_count, community_reg.id, community_reg.code, community_reg.name.encode("utf-8"))
+
+        parent_id = None
+        if community_reg.parent_id:
+            parent_id = community_reg.parent_id.id
+
+        alias = None
+        if community_reg.alias:
+            alias = community_reg.alias
+
+        comm_location = None
+        if community_reg.comm_location:
+            comm_location = community_reg.comm_location
+
+        user_id = None
+        if community_reg.user_id:
+            user_id = community_reg.user_id.id
+
+        notes = None
+        if community_reg.notes:
+            notes = community_reg.notes
+
+        cursor.execute('''
+            INSERT INTO ''' + table_name + '''(
+                id,
+                tag_ids,
+                category_ids,
+                parent_id,
+                name,
+                alias,
+                code,
+                comm_location,
+                date_inclusion,
+                user_id,
+                notes,
+                active,
+                active_log
+                )
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)
+            ''', (community_reg.id,
+                  str(community_reg.tag_ids.id),
+                  str(community_reg.category_ids.id),
+                  parent_id,
+                  community_reg.name,
+                  alias,
+                  community_reg.code,
+                  comm_location,
+                  community_reg.date_inclusion,
+                  user_id,
+                  notes,
+                  community_reg.active,
+                  community_reg.active_log,
+                  )
+        )
+
+    conn.commit()
+    conn.close()
+
+    print()
+    print('--> community_count: ', community_count)
